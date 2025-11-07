1da40e63ee8253ebaf0f5afe8dfd516cc3cceedb
Igor <igor.gordeev.06@inbox.ru>
2025-11-07 18:32:09 +0500

    добавить переменные набора пользовательского интерфейса и экспортировать из библиотеки

---
+++ ui-kit/src/index.ts
@@ -0,0 +1,5 @@
+// export { Button } from './components/Button';
+// export { Input } from './components/Input';
+// export { CodeInput } from './components/CodeInput';
+
+export { default as variables } from './styles/variables.scss'
