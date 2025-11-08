6fec92c6af011806306fd7282efd541082886002
Igor <igor.gordeev.06@inbox.ru>
2025-11-08 17:42:44 +0500

    создать компонент ввода с label, icon, error states

--- ui-kit/src/index.ts
+++ ui-kit/src/index.ts
@@ -5,3 +5,5 @@
 // ui-kit/src/index.ts
 export { default as Button } from './components/Button/ui/Button';
 export { default as variables } from './styles/variables.scss';
+export { Input } from './components/Input'
+export { EyeIcon } from './icons';
