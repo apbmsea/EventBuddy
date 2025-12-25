cf173aaca0f3d0edebb0ef0b951756fe7ab1175e
Igor <igor.gordeev.06@inbox.ru>
2025-12-25 22:28:24 +0500

    initialize UI-Kit library

---
+++ ui-kit/src/index.ts
@@ -0,0 +1,13 @@
+// export { Button } from './components/Button';
+// export { Input } from './components/Input';
+// export { CodeInput } from './components/CodeInput';
+
+
+
+// ui-kit/src/index.ts
+export { default as Button } from './components/Button';
+export { default as Input } from './components/Input';
+export { default as CodeInput } from './components/CodeInput';
+export { default as Select } from './components/Select';
+
+export { default as EyeIcon } from './icons/EyeIcon';
